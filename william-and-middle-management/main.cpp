4a26a87208d70efeaaa4d003369c171f66970755
experimnetallife <user@experimentallife.dev>
2022-11-01 07:52:18 -0500

    non funciona :v

--- william-and-middle-management/main.cpp
+++ william-and-middle-management/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <cstdlib>
 
 int main() {
+  int d, 
+      s = 0, 
+      r = 0, a, b;
+  float t, rv = 0, av, bv;
+
+  while(std::cin >> d) {
+    if(!s)
+      a = d;
+    
+    if (s == 1) {
+      av = a;
+      av /= d;
+      a *= d;
+    }
+// ---
+    if (s == 2)
+      b = d;
+
+    if (s == 3) {
+      bv = b;
+      bv /= d;
+      b *= d;
+
+      t = av + bv;
+      if (t > rv) {
+        rv = t;
+        r = a + b;
+      }
+      
+      a = b;
+      av = bv;
+      s = 2;
+    } else s++;  
+  }
+
+  std::cout << r;
 
   return 0;
 }
